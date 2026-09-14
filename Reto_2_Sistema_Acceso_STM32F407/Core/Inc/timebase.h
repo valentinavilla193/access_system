a363664b63ed5e4faf0641bfc85c458d50ab6b77
Valentina Villa <valentinavilla193@gmail.com>
2026-09-13 20:33:54 -0500

    Fix comment formatting in timebase.h

--- Reto_2_Sistema_Acceso_STM32F407/Core/Inc/timebase.h
+++ Reto_2_Sistema_Acceso_STM32F407/Core/Inc/timebase.h
@@ -3,11 +3,11 @@
 
 #include "stm32f4xx.h"
 
-/* Codigo BASE del curso (sin modificar): base de tiempo no bloqueante
+/* Codigo BASE del curso: base de tiempo no bloqueante
  * por SysTick a 1 ms (asume reloj del sistema a 16 MHz). */
 
 extern volatile uint32_t ms_ticks;
 
 void SysTick_Init(void);
 
-#endif /* TIMEBASE_H */
+#endif 
