ed9ed2c09f3c73484deb6d27141a834455bb5c66
Valentina Villa <valentinavilla193@gmail.com>
2026-09-13 20:35:54 -0500

    Refactor comments in timebase.h
    Updated comments for clarity and removed unnecessary lines.

--- Reto_2_Sistema_Acceso_STM32F407/Core/Inc/timebase.h
+++ Reto_2_Sistema_Acceso_STM32F407/Core/Inc/timebase.h
@@ -3,11 +3,10 @@
 
 #include "stm32f4xx.h"
 
-/* Codigo BASE del curso: base de tiempo no bloqueante
- * por SysTick a 1 ms (asume reloj del sistema a 16 MHz). */
+// Base de tiempo no bloqueante por SysTick a 1 ms (asume reloj del sistema a 16 MHz).
 
 extern volatile uint32_t ms_ticks;
 
 void SysTick_Init(void);
 
-#endif 
+#endif
